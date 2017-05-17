556b36f4229178f5ebaba94ae57d1d3d16123a97
Simon Amor <simonrramor@gmail.com>
2017-05-17 21:05:28 +0100

    almost finished, cant remember all the changed since last commit sorry

---
+++ js/remove-mobile.js
@@ -0,0 +1,15 @@
+// console.log('heyOOOOO')
+//
+// $(window).resize(function () {
+//   const viewportWidth = $(window).width();
+//   if (viewportWidth < 600) {
+//     $('body').addClass('remove');
+//   } else $('body').removeClass('remove');
+// });
+//
+// $(window).resize(function () {
+//   const viewportWidth = $(window).width();
+//   if (viewportWidth < 600) {
+//     $('body').addClass('remove');
+//   } else $('body').removeClass('remove');
+// });
--- js/title-change.js
+++ js/title-change.js
@@ -1,20 +1,12 @@
-
-      $(function() {
-        // Get page title
-          var pageTitle = $("title").text();
-
-
-        // Change page title on blur
-        $(window).blur(function() { 
-          $("title").text("WAIT! COME BACK! - " + pageTitle);
+$(function() {
+          var pageTitle = $('title').text();
+        $(window).delay(18000).blur(function () {
+          $('title').text(`WAIT! COME BACK! ${pageTitle}`)
         });
-
-        // Change page title back on focus
         $(window).focus(function() {
-          $("title").text(pageTitle);
+          $('title').text(pageTitle);
         });
       });
-
 
 
       
