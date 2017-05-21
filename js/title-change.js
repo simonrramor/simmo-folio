78d559b5fb20e763fe653a285f765c621f8576d1
Simon Amor <simonrramor@gmail.com>
2017-05-21 10:51:58 +0100

    folio ready for first applications

--- js/title-change.js
+++ js/title-change.js
@@ -1,9 +1,13 @@
-setTimeout$(function() {
-          var pageTitle = $('title').text();
-        $(window).delay(18000).blur(function () {
-          $('title').text(`WAIT! COME BACK! ${pageTitle}`)
-        });
-        $(window).focus(function() {
-          $('title').text(pageTitle);
-        });
-      });
+$(() => {
+  const setTimeout = function setTimeout() {
+    var pageTitle = $('title').text();
+    $(window).blur(function () {
+      $('title').text(`WAIT! COME BACK! ${pageTitle}`);
+    });
+    $(window).focus(function() {
+      $('title').text(pageTitle);
+    });
+  };
+
+  setTimeout();
+});
